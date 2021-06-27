90bcc91ef7ae35977759f481229c173eb73361cb
lmagoncalo <lmagoncalo@hotmail.com>
2021-06-27 23:29:09 +0100

    Fake data

--- api/server.py
+++ api/server.py
@@ -1,13 +1,15 @@
 import argparse
+from os import path
 
+import pickle
 from flask import Flask, request
 from flask_cors import CORS
-from tinydb import TinyDB
+# from tinydb import TinyDB
 
 app = Flask(__name__)
 CORS(app)
 
-db = TinyDB('db.json')
+# db = TinyDB('db.json')
 real_time = {}
 
 
@@ -21,6 +23,10 @@ def api():
 	try:
 		data = request.json
 		real_time[str(data['ip'])] = data
+
+		with open("info.pkl", 'wb') as handle:
+			pickle.dump(real_time, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 		# db.insert(data)
 		return 'OK'
 	except Exception as e:
@@ -32,5 +38,10 @@ if __name__ == '__main__':
 	parser.add_argument("-ip", help="Ip address", type=str, default="localhost")
 	parser.add_argument("-port", help="Port", type=int, default=5000)
 	args = parser.parse_args()
+
+	if path.isfile("info.pkl"):
+		with open("info.pkl", 'rb') as handle:
+			real_time = pickle.load(handle)
+			print("File loaded")
 
 	app.run(host=args.ip, port=args.port)
